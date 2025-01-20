3877f3d4f87fdc698da5a7f5f24767f28675655e
ruthgrace <ruthgracewong@gmail.com>
2025-01-20 07:18:50 +0000

    get htmx to work on make poll page

--- website.py
+++ website.py
@@ -1,19 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
-def homepage():
+def home_page():
     return render_template('home.html.j2')
 
 @app.route("/makepoll")
-def makepollpage():
+def make_poll_page():
     return render_template('makepoll.html.j2')
 
 @app.route("/vote")
-def pollpage():
+def poll_page():
     return render_template('poll.html.j2')
 
 @app.route("/results")
-def pollresultspage():
+def poll_results_page():
     return render_template('pollresults.html.j2')
+
+@app.route("/pollsubmit", methods=["POST"])
+def newpoll():
+    candidates_text = request.form.get("candidates", "")
+    seats = request.form.get("seats", "0")
+    seats = int(seats)
+    candidates = candidates_text.splitlines()
+
+    return f"""
+    <h2>Poll Created!</h2>
+    <p>You entered {len(candidates)} candidate(s): {candidates}</p>
+    <p>Number of seats: {seats}</p>
+    """
