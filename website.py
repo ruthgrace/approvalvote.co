ade99509ce2af04bcb81ab75ed8731f44b0c3c84
ruthgrace <ruthgracewong@gmail.com>
2025-01-25 07:12:51 +0000

    register user flow for if someone tries to vote who isn't registered yet

--- website.py
+++ website.py
@@ -15,6 +15,11 @@ CANDIDATES_TEXT = "candidates_text"
 SEATS = "seats"
 NEW_POLL = "new_poll"
 NEW_VOTE = "new_vote"
+
+SELECTED = "selected"
+ID = "id"
+EMAIL = "email"
+
 @app.route("/")
 def home_page():
     return render_template('home.html.j2')
@@ -34,9 +39,18 @@ def user_exists(email, supabase):
         return False
     return True
 
+def update_form_data(poll_data, supabase):
+    # delete their form data entry if they had any left over
+    supabase.table("FormData").delete().eq("email", poll_data[EMAIL]).execute()
+    # write new form data entry
+    response = (
+        supabase.table("FormData")
+        .insert({"email": poll_data[EMAIL], "form_data": poll_data})
+        .execute()
+    )
 
 @app.route("/vote/<int:poll_id>")
-def poll_page():
+def poll_page(poll_id):
     candidates = []
     seats = 0
     try:
@@ -50,37 +64,47 @@ def poll_page():
     return render_template('poll.html.j2', seats=seats, candidates=candidates, poll_id=poll_id)
 
 @app.route("/votesubmit", methods=["POST"])
-def new_vote():
-    selected = request.form.getlist("poll_option")
-    poll_id = request.form.get("poll_id")
-    email = request.form.get("user_email")
-    selected_str = ""
-    if len(selected) == 0:
+def new_vote(form_data=None):
+    poll_data = {}
+    if form_data is not None:
+        poll_data = form_data
+    else:
+        poll_data[SELECTED] = request.form.getlist("poll_option")
+        poll_data[ID] = request.form.get("poll_id")
+        poll_data[EMAIL] = request.form.get("user_email")
+    if len(poll_data[SELECTED]) == 0:
         return f"""
         <h2>Your vote was not counted. Please select at least one option.</h2>
         """
     try:
         supabase: Client = create_client(constants.DB_URL, constants.DB_SERVICE_ROLE_KEY)
-        user_id = get_user_id(email, supabase)
+        if not user_exists(poll_data[EMAIL], supabase):
+            print(f"user does not exist based on email {poll_data[EMAIL]}")
+            update_form_data(poll_data, supabase)
+            return render_template("new_user_snippet.html.j2", email=poll_data[EMAIL], origin_function=NEW_VOTE)
+        user_id = get_user_id(poll_data[EMAIL], supabase)
         # if user does not exist, create it
-        response = supabase.table("PollOptions").select("option").eq("poll", poll_id).execute()
-        for option_id in selected:
+        response = supabase.table("PollOptions").select("option").eq("poll", poll_data[ID]).execute()
+        option_names = []
+        for option in poll_data[SELECTED]:
+            (option_id, option_name) = option.split("|", maxsplit=1)
+            option_names.append(option_name)
             response = (
                 supabase.table("Votes")
-                .insert({"poll": poll_id, "option": option_id, "user": user_id})
+                .insert({"poll": poll_data[ID], "option": option_id, "user": user_id})
                 .execute()
             )
             print(f"insert vote response: {response}")
     except Exception:
         print(traceback.format_exc())
-    if len(selected) == 1:
+    if len(option_names) == 1:
         return f"""
         <h2>Vote submitted!</h2>
-        <p>You voted for: {selected[0]}</p>
+        <p>You voted for: {option_names[0]}</p>
         """
     return f"""
     <h2>Vote submitted!</h2>
-    <p>You voted for: {", ".join(selected[:-1])}, and {selected[-1]}</p>
+    <p>You voted for: {", ".join(option_names[:-1])}, and {option_names[-1]}</p>
     """
 
 @app.route("/results")
@@ -147,14 +171,7 @@ def new_poll(form_data=None):
         supabase: Client = create_client(constants.DB_URL, constants.DB_SERVICE_ROLE_KEY)
         if not user_exists(poll_data[EMAIL], supabase):
             print(f"user does not exist based on email {poll_data[EMAIL]}")
-            # delete their form data entry if they had any left over
-            supabase.table("FormData").delete().eq("email", poll_data[EMAIL]).execute()
-            # write new form data entry
-            response = (
-                supabase.table("FormData")
-                .insert({"email": poll_data[EMAIL], "form_data": poll_data})
-                .execute()
-            )
+            update_form_data(poll_data, supabase)
             return render_template("new_user_snippet.html.j2", email=poll_data[EMAIL], origin_function=NEW_POLL)
         print("getting user id")
         user_id = get_user_id(poll_data[EMAIL], supabase)
