e85683973d2cbbd467fc340e9b1ab4070be13491
ruthgrace <ruthgracewong@gmail.com>
2025-03-01 23:54:17 +0000

    first AI pass at refactoring to make more eaisly unit testable

---
+++ email_service.py
@@ -0,0 +1,43 @@
+import random
+import smtplib
+import ssl
+from email.message import EmailMessage
+
+class EmailService:
+    def __init__(self, noreply_email, noreply_password):
+        self.noreply_email = noreply_email
+        self.noreply_password = noreply_password
+        self.DIGITS = "0123456789"
+        self.VERIFICATION_CODE_LENGTH = 4
+
+    def generate_verification_code(self):
+        return "".join([random.choice(self.DIGITS) for _ in range(self.VERIFICATION_CODE_LENGTH)])
+
+    def send_verification_email(self, recipient_email):
+        code = self.generate_verification_code()
+        message = EmailMessage()
+        message["Subject"] = "ApprovalVote.Co Verification Code"
+        message["From"] = self.noreply_email
+        message["To"] = recipient_email
+        message.set_content(f"""
+Hello,
+
+Your verification code for ApprovalVote.Co is: {code}
+
+If you didn't request this code, please ignore this email.
+        """.strip())
+
+        self._send_email(message)
+        return code
+
+    def _send_email(self, message):
+        smtp_server = "smtp.gmail.com"
+        port = 587
+        context = ssl.create_default_context()
+
+        with smtplib.SMTP(smtp_server, port) as server:
+            server.ehlo()
+            server.starttls(context=context)
+            server.ehlo()
+            server.login(self.noreply_email, self.noreply_password)
+            server.send_message(message) 
